e34da99c4ca312e6125d5059cad739a8f92ab7b7
Christerle Nounoute <christerlesn@gmail.com>
2020-09-24 21:17:35 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,12 +2,13 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-BRANDS = ["Uggs", "Rainbow", "Nike", "Nike"]
+BRANDS = []
 
   def initialize(brand)
     @brand = brand
     if !(BRANDS.include?(@brand))
       BRANDS << @brand
+    end
   end
 
   def cobble
