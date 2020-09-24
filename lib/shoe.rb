5e477eecfc82d27522d72b970a0a5a679fc2af71
Christerle Nounoute <christerlesn@gmail.com>
2020-09-24 21:09:32 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
 
-BRANDS = ["Uggs", "Rainbow", "Nike", "Nike"]
+BRANDS = ["Uggs", "Rainbow"]
 
   def initialize(brand)
     @brand = brand
